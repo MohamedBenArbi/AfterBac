6350428629161bd4e5bba5fc0bcecd0243cf1ad3
MohamedBenArbi <mohamedbenarbi01@gmail.com>
2023-02-14 20:57:13 +0100

    uni routes added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const router = require('./routes/uniRoutes')
 const port = process.env.PORT || 5000
 app = express()
 
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-})
+app.use('/api/uni',router)
 
 app.listen(port ,()=>{ console.log(`Server started on ${port}`)})
